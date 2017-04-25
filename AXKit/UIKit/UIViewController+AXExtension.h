f83ac54c856612a257862db96d470990b388552c
xaoxuu <xaoxuu@gmail.com>
2017-04-25 11:32:45 +0800

    note

--- AXKit/UIKit/UIViewController+AXExtension.h
+++ AXKit/UIKit/UIViewController+AXExtension.h
@@ -10,10 +10,31 @@
 NS_ASSUME_NONNULL_BEGIN
 @interface UIViewController (AXExtension)
 
+/**
+ push view controller
+
+ @param vcName view controller name
+ @param animated animated
+ */
 - (void)ax_pushViewControllerNamed:(NSString *)vcName animated:(BOOL)animated;
 
+/**
+ push view controller
+ 
+ @param vcName view controller name
+ @param animated animated
+ @param completion completion
+ */
 - (void)ax_pushViewControllerNamed:(NSString *)vcName animated:(BOOL)animated completion:(void (^)(UIViewController *targetVC))completion;
 
+/**
+ push view controller
+ 
+ @param vcName view controller name
+ @param animated animated
+ @param completion completion
+ @param fail error info
+ */
 - (void)ax_pushViewControllerNamed:(NSString *)vcName animated:(BOOL)animated completion:(void (^)(UIViewController *targetVC))completion fail:(void (^)(NSError *error))fail;
 
 @end
