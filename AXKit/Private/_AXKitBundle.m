24b03b5dd8ad9e0fbab9580203e3c64a7de071bb
xaoxuu <xaoxuu@gmail.com>
2017-06-29 19:11:25 +0800

    NSLocalizedStringFromAXKit

--- AXKit/Private/_AXKitBundle.m
+++ AXKit/Private/_AXKitBundle.m
@@ -11,10 +11,19 @@
 static NSBundle *bundle = nil;
 
 inline NSString *NSLocalizedStringFromAXKit(NSString *key){
+    // @xaoxuu: in main bundle
     if (!bundle) {
         bundle = [NSBundle bundleWithPath:[[NSBundle mainBundle] pathForResource:@"AXKit" ofType:@"bundle"]];
     }
-    return NSLocalizedStringFromTableInBundle(key, nil, bundle, nil);
+    
+    // @xaoxuu: in AXKit.framework/AXKit.bundle
+    if (!bundle) {
+        NSString *path_frameworks = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"Frameworks"];
+        bundle = [NSBundle bundleWithPath:[path_frameworks stringByAppendingPathComponent:@"AXKit.framework"]];
+        bundle = [NSBundle bundleWithPath:[bundle pathForResource:@"AXKit" ofType:@"bundle"]];
+    }
+    
+    return NSLocalizedStringFromTableInBundle(key, nil, bundle, nil) ?: key;
 }
 
 @implementation _AXKitBundle
