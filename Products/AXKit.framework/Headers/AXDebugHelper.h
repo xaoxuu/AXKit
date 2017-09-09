6dfb0e2ccbe0aac9fe50318f03cfdc67096f1afc
xaoxuu <xaoxuu@gmail.com>
2017-09-09 17:39:59 +0800

    sh

--- Products/AXKit.framework/Headers/AXDebugHelper.h
+++ Products/AXKit.framework/Headers/AXDebugHelper.h
@@ -7,11 +7,21 @@
 //
 
 #import <Foundation/Foundation.h>
-
+NS_ASSUME_NONNULL_BEGIN
 /**
  只在debug模式下运行的函数
  
- @param debug debug操作
+ @param debug debug模式下执行的代码
  */
-FOUNDATION_EXTERN void ax_debug_only(void (^debug)());
+FOUNDATION_EXTERN void ax_debug_only(void (^ _Nullable debug)());
 
+
+/**
+ debug和release模式的宏
+
+ @param debug debug模式下执行的代码
+ @param release release模式下执行的代码
+ */
+FOUNDATION_EXTERN void ax_debug_release(void (^ _Nullable debug)(), void (^ _Nullable release)());
+
+NS_ASSUME_NONNULL_END
