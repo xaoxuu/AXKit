e3179c294170c8b50087253346087a58e16d464d
xaoxuu <xaoxuu@gmail.com>
2017-10-09 11:45:00 +0800

    demo

--- AXKitDemo/classes/controllers/debug/DebugTV.m
+++ AXKitDemo/classes/controllers/debug/DebugTV.m
@@ -11,6 +11,18 @@
 @implementation DebugTV
 
 
-
+- (void)indexPath:(NSIndexPath *)indexPath didSelected:(__kindof BaseTableModelRow *)model{
+    NSInteger section = indexPath.section;
+    NSInteger row = indexPath.row;
+    if (section == 0) {
+        if (row == 8) {
+            [NSUserDefaults ax_readStringForKey:@"123" completion:^(NSString * _Nonnull string) {
+                AXLogSuccess();
+            } failure:^(NSError * _Nonnull error) {
+                AXLogFailure();
+            }];
+        }
+    }
+}
 
 @end
